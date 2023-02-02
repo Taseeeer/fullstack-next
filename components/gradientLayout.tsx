c0fbed7e9c27ad5b969d4769551ad4e93db58b23
Syed Taseer Shah <66362477+Taseeeer@users.noreply.github.com>
2023-02-02 15:14:11 +0500

    home design gradient

---
+++ components/gradientLayout.tsx
@@ -0,0 +1,17 @@
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+
+export default function GradientLayout({ color, children, image, subtitle, title, description, roundImage }) {
+    return <Box height="calc(100vh - 100px)" overflowY="scroll" bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}>
+        <Flex bg={`${color}.600`} padding="40px" align="end">
+            <Box padding="20px">
+                <Image src={image} boxShadow="2xl" boxSize="160px" borderRadius={roundImage ? "100%": "3px"} />
+            </Box>
+
+            <Box padding="20px" lineHeight="40px" color="white">
+                <Text fontSize="sm" fontWeight="bold" casing="uppercase">{ subtitle }</Text>
+                <Text fontSize="6xl" fontWeight="bold" casing="uppercase">{ title }</Text>
+                <Text fontSize="sm" fontWeight="100">{ description }</Text>
+            </Box>
+        </Flex>
+    </Box>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import GradientLayout from '@/components/gradientLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-    Home
-    </>
-  )
+  return <GradientLayout image="https://i.pinimg.com/originals/4d/b0/71/4db0719744d73a1ad88e0f9fc1db649b.jpg" color="blue" subtitle="Listen to it" title="Taseer" description="whatever whateverwhateverwhateverwhateverwhatever">
+    <div>Home</div>
+  </GradientLayout> 
 }
